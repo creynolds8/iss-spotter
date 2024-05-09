8532df6653311dbc3ff7fdb781e4e94d67b78b9d
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 16:33:16 -0700

    add success/fail logic

---
+++ index.js
@@ -0,0 +1,9 @@
+const { fetchMyIP } = require('./iss');
+
+fetchMyIP((err, ip) => {
+  if (err) {
+    console.log('failed');
+    return
+  }
+  console.log(ip);
+})
--- iss.js
+++ iss.js
@@ -1,5 +1,14 @@
-const fetchMyIP = function(callback) {
+const needle = require('needle')
 
+const fetchMyIP = function(callback) {
+  needle.get('https://api.ipify.org?format=json', (err, response, body) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    const ip = JSON.stringify(body);
+    callback(null, ip);
+  })
 };
 
 module.exports = { fetchMyIP };
