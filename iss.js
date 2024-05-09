c2ac01625d67af6881f701cdf60622c0fd70f2f4
creynolds8 <cameron8.cr@gmail.com>
2024-05-09 16:40:19 -0700

    end of ISS spotter I

--- iss.js
+++ iss.js
@@ -2,16 +2,13 @@ const needle = require('needle');
 
 const fetchMyIP = function(callback) {
   needle.get('https://api.ipify.org?format=json', (err, response, body) => {
-    if (err) {
-      callback(err);
-      return;
-    }
+    if (err) return callback(err, null);
     if (response.statusCode !== 200) {
       const msg = `Status code: ${response.statusCode} when fetching IP. Response: ${body}.`;
       callback(Error(msg), null);
       return;
     }
-    const ip = JSON.stringify(body);
+    const ip = body.ip
     callback(null, ip);
   });
 };
